317d68cd48819dba48dd54fd515537fd999c5e30
jadhavanushka <104165311+jadhavanushka@users.noreply.github.com>
2024-08-14 00:50:31 +0530

    Update 404

--- src/Pages/NoPage.js
+++ src/Pages/NoPage.js
@@ -1,12 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
 import errorillustration from '../Images/404-error-illustration.jpg';
+import { color } from "chart.js/helpers";
 
 const noPage = () => {
     return (
         <>
         <div style={{display:"inline-flex",flexDirection:"column", justifyContent:"center"}}>
         <img src={errorillustration} alt="404" style={{width:"600px"}}/>
-        <button style={{backgroundColor:"#E8FFAF", border:"#1B2E35 1px solid"}}><Link to="/">Go back to home</Link></button>
+        <button className="visualize-btn"><Link to="/" style={{color:"#fff"}}>Go back to home</Link></button>
         </div>
         <Outlet />
     </>
